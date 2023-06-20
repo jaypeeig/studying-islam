f9437cbcd8affdb8774b8eaf8da33a09c5503f84
Jaypee <jignacio@cambridge.org>
2023-06-20 20:10:29 +0800

    Update desc content

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'Studying Islam';
-export const SITE_DESCRIPTION = 'The project encompasses comprehensive knowledge derived from the study of Islam.';
+export const SITE_DESCRIPTION = 'This website project serves as a comprehensive documentation, encompassing all the knowledge acquired through the study of Islam. It aims to provide a comprehensive resource for anyone seeking to explore and understand the teachings and principles of Islam.';
