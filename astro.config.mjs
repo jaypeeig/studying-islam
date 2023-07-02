545481f86dc853c288f1aa6094dbc16113c3851c
jaypeeig <jaypeeig@gmail.com>
2023-07-02 22:44:21 +0800

    Add tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://studying-islam.pages.dev',
-	integrations: [mdx(), sitemap()],
+  site: 'https://studying-islam.pages.dev',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
